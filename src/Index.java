0bd4c917ab23254f3a04985e70c8f68a274e10e9
Elliot Lichtman <elliot.joseph.lichtman@gmail.com>
2022-09-11 22:16:42 -0700

    Creating "index" File

--- src/Index.java
+++ src/Index.java
@@ -1,4 +1,9 @@
 import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.HashMap;
 
 public class Index {
@@ -8,7 +13,7 @@ public class Index {
 	public Index () {
 		
 		System.out.println("starting");
-		File f1 = new File("/Honors Topics Git/testFolder");
+		File f1 = new File("/Honors Topics Git/objects");
 		
 		boolean bool = f1.mkdirs();
 		
@@ -18,6 +23,14 @@ public class Index {
 		}
 		else {
 			System.out.println("failed");
+		}
+		
+		Path filePathToWrite = Paths.get("./objects/index");
+		try {
+			Files.writeString(filePathToWrite, "", StandardCharsets.ISO_8859_1);
+		}
+		catch (IOException exception) {
+			System.out.println("Write failed");
 		}
 	}
 	
